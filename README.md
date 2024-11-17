b0d80717b346dd449cb0c5d7fcd4a8907bd7328c
Kirill <sobolevkirill0@gmail.com>
2024-11-17 16:33:55 +0300

    Training to use data transfer from the controller to the view

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 In this repository I learning Spring MVC
 
-Commit №1 Creating project with thymeleafe and add DispatcherServlet for url /hello-world
+Commit №1 Creating project with thymeleaf and add DispatcherServlet for url /hello-world
 
 Commit №2 Delete xml configuration and add full Java-based configuration
 
 Commit №3 Training to use Annotation @RequestMapping and multiple Controller
 
-Commit #4 Training to use Annotation @RequestParam in controller and view
+Commit №4 Training to use Annotation @RequestParam in controller and view
+
+Commit №5 Training to use data transfer from the controller to the view with use Model and Thymeleaf
--- src/main/java/com/sobolev/spring/controllers/FirstController.java
+++ src/main/java/com/sobolev/spring/controllers/FirstController.java
@@ -13,11 +13,36 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 
-        System.out.println("Hello " + name + " " + surname);
+//        System.out.println("Hello " + name + " " + surname);
+        model.addAttribute("message", "Hello " + name + " " + surname );
         return "first/hello";
     }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a", required = false) int a,
+                             @RequestParam(value = "b", required = false) int b,
+                             @RequestParam(value = "action", required = false) String action,
+                             Model model) {
+        switch (action) {
+            case "multiplication":
+                model.addAttribute("result", a*b);
+                break;
+            case "division":
+                model.addAttribute("result", a / (double) b);
+                break;
+            case "addition":
+                model.addAttribute("result", a+b);
+                break;
+            case "subtraction":
+                model.addAttribute("result", a-b);
+                break;
+        }
+        return "first/calculator";
+    }
+
     @GetMapping("/goodbye")
     public String goodByePage(){
         return "first/goodbye";
